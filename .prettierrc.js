3b5ecb6009b4ef52c6c6c52eb1d55e3cb25a5bae
kobezzza <kobezzza@gmail.com>
2020-07-06 07:47:21 +0300

    Added disclaimer

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,13 @@
+'use strict';
+
+/*!
+ * V4Fire Linters
+ * https://github.com/V4Fire/Linters
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Linters/blob/master/LICENSE
+ */
+
 module.exports = {
 	singleQuote: true,
 	trailingComma: 'none',
